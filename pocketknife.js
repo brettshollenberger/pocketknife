815eb5ab320b7ee2b4412c69a71ffa0d5a6f2b68
Brett Shollenberger <brett.shollenberger@gmail.com>
2013-11-16 22:29:16 -0500

    Add mixin function

--- pocketknife.js
+++ pocketknife.js
@@ -45,4 +45,75 @@
             return fBound;
         };
     }
+    
+    var pk = {};
+
+    pk.capitalize = function (string) {
+        return string.charAt(0).toUpperCase() + string.substring(1).toLowerCase();
+    };
+
+    pk.mixin = function(receiver, giver) {
+      for (var i in giver) {
+        if (!receiver.hasOwnProperty(i)) {
+          Object.defineProperty(receiver, i, giver[i]);
+        }
+      }
+    };
+
+    var enumerable = {
+      all: {
+        enumerable: false,
+        value: function(cb) {
+          var truth = true;
+          this.each(function(i) { if (!cb(i)) truth = false; });
+          return truth;
+        }
+      },
+      any: {
+        enumerable: false,
+        value: function(cb) {
+          var truth = false;
+          this.each(function(i) { if (cb(i)) truth = true; });
+          return truth;
+        }
+      },
+      collect: {
+        enumerable: false,
+        value: function(cb) {
+          var returner = [];
+          this.each(function(i) { returner.push(cb(i)); });
+          return returner;
+        }
+      },
+      first: {
+        enumerable: false,
+        value: function(num) {
+          var returner = [];
+          var num = num || 1;
+          var count = 1;
+          this.each(function(i) { if (count <= num) returner.push(i); count++; });
+          return returner;
+        }
+      },
+      include: {
+        enumerable: false,
+        value: function(what) {
+          var truth = false;
+          this.each(function(i) { if (i == what) truth = true; });
+          return truth;
+        }
+      }
+    };
+
+    Object.defineProperty(Array.prototype, 'each', {
+      enumerable: false,
+      value: function(cb) {
+        for (var i in this) {
+          cb(this[i]);
+        }
+      }
+    });
+
+    _.mixin(pk);
+
 }).call(this);
